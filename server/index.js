8afbd5ded9f915d866e6306f8b903e6fead0d944
Karthik-1175 <karthikgandham117@gmail.com>
2026-02-26 20:30:10 +0530

    changed CORS

--- server/index.js
+++ server/index.js
@@ -9,17 +9,32 @@ import authRoutes from "./routes/authRoutes.js";
 import { protect } from "./middleware/authMiddleware.js";
 import studyRoutes from "./routes/studyRoutes.js";
 
-
-
 const app = express();
+
 console.log("GROQ KEY:", process.env.GROQ_API_KEY);
-// Middleware
+
+// ✅ CORS CONFIG (IMPORTANT)
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://ai-study-partner-sable.vercel.app",
+];
+
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
   })
 );
+
+// Middleware
 app.use(express.json());
 
 // Test route
@@ -35,10 +50,13 @@ app.get("/api/protected", protect, (req, res) => {
   });
 });
 
-const PORT = process.env.PORT || 5000;
-connectDB();
+// Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/study", studyRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+connectDB();
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
