ed6baf196a5a2c5772dbdcb0587689bfcebe7b78
Karthik-1175 <karthikgandham117@gmail.com>
2026-02-26 20:11:40 +0530

    update backend url to production

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://aistudypartner-production.up.railway.app",
 });
 
 // 🔥 Attach token automatically
